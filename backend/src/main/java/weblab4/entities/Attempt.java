aaa20646190f50e78ef1848936a35411872e476e
Mariya Senina <senina.m.m@gmail.com>
2022-01-07 09:46:55 +0300

    database setup

---
+++ backend/src/main/java/weblab4/entities/Attempt.java
@@ -0,0 +1,66 @@
+package weblab4.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.java.Log;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Log
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Attempt{
+
+    @OneToOne(mappedBy = "attempt", cascade = CascadeType.ALL)
+    @JoinColumn(name = "coordinates", nullable = false)
+    private Coordinates coordinates = new Coordinates(0.0, 0.0, 1.0); //fixme set r = 0.0
+    private boolean doFitArea = true;
+
+    @Id @PrimaryKeyJoinColumn
+    @JsonIgnore
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id = null;
+
+    @JsonIgnore
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Owner owner = null;
+
+    public Attempt(Coordinates coordinates, boolean doFitArea) {
+        this.coordinates = coordinates;
+        this.doFitArea = doFitArea;
+        this.owner = null;
+        this.id = null;
+    }
+
+    public static Attempt initAttempt() {
+        Attempt attempt = new Attempt();
+        attempt.setCoordinates(new Coordinates());
+        attempt.setOwner(Owner.initOwner());
+        return attempt;
+    }
+
+    @Override
+    public String toString() {
+        return "Attempt{" +
+                "coordinates: (x=" + coordinates.getX() +
+                "; y=" + coordinates.getY() +
+                "; r=" + coordinates.getR() +
+                "), doFitArea=" + doFitArea +
+                ", id=" + id +
+                ", owner isn't null=" + (Objects.nonNull(owner)) +
+                " " + System.currentTimeMillis() +
+                '}';
+    }
+
+    public void setCoordinates(Coordinates coordinates) {
+        log.info("Timestamp(setCoordinates) " + System.currentTimeMillis() + " " + coordinates.toString());
+        this.coordinates = coordinates;
+    }
+}
---
+++ backend/src/main/java/weblab4/entities/Owner.java
@@ -0,0 +1,38 @@
+package weblab4.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+public class Owner {
+    @Id
+    @Column(name = "userSessionId", nullable = false)
+    private String sessionId;
+    @JsonIgnore
+    //if we want to drop all attempts after owner death
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Attempt> attemptList;
+
+    public static Owner initOwner(){
+        Owner owner = new Owner();
+        owner.setAttemptList(new ArrayList<>());
+        return owner;
+    }
+
+    @Override
+    public String toString() {
+        return "Owner{" +
+                "sessionId=" + sessionId +
+                ", attemptsIds=" + attemptList.stream().map(attempt -> attempt.getId().toString() + ", ") +
+                '}';
+    }
+}
