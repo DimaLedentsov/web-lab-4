ceb6ebe4e08444b4700dc0eaaed74f46d4b4822e
Mariya Senina <senina.m.m@gmail.com>
2022-01-22 20:16:29 +0300

    add note to try default logout

--- backend/src/main/java/weblab4/sequrity/WebSecurity.java
+++ backend/src/main/java/weblab4/sequrity/WebSecurity.java
@@ -28,7 +28,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(SIGN_UP_URL).permitAll()
                 .anyRequest().authenticated()
-
+//                .and().logout() //fixme: try how default logout works
                 .and()
                 .addFilter(new JWTAuthorizationFilter(authenticationManager()))
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
